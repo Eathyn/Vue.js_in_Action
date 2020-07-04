cf7143ba4e61e9c84fb8f09f3a42be5f245a91c1
Eathyn <eathynzakas@gmail.com>
2020-07-04 13:35:53 +0800

    chapter 05

---
+++ chapter-05/index.js
@@ -0,0 +1,111 @@
+let APP_LOG_LIFECYCLE_EVENTS = true;
+let webstore = new Vue({
+    el: '#app',
+    data: {
+        sitename: "Vue.js Pet Depot",
+        showProduct: true,
+        a: false,
+        states: {
+            AL: 'Alabama',
+            AK: 'Alaska',
+            AR: 'Arizona',
+            CA: 'California',
+            NV: 'Nevada'
+        },
+        order: {
+            firstName: '',
+            lastName: '',
+            address: '',
+            city: '',
+            zip: '',
+            state: '',
+            method: 'Home Address',
+            business: 'Business Address',
+            home: 'Home Address',
+            gift:'Send As A Gift',
+            sendGift: 'Send As A Gift',
+            dontSendGift: 'Do Not Send As A Gift'
+        },
+        products: [],
+        product: {
+            id: 1001,
+            title: "Cat Food, 25lb bag",
+            description: "A 25 pound bag of <em>irresistible</em>, organic goodness for your cat.",
+            price: 2000,
+            image: "assets/images/product-fullsize.png",
+            availableInventory: 10,
+            rating: 3,
+        },
+        cart: [],
+    },
+    methods: {
+        addToCart: function(aProduct) {
+            this.cart.push(aProduct.id);
+        },
+        showCheckout() {
+            this.showProduct = this.showProduct ? false: true;
+        },
+        submitForm() {
+            alert('Submitted');
+        },
+        checkRating(n, myProduct) {
+            return myProduct.rating - n >= 0;
+        },
+        canAddToCart(aProduct) {
+            return aProduct.availableInventory > this.cartCount(aProduct.id);
+        },
+        cartCount(id) {
+            let count = 0;
+            for (let i = 0; i < this.cart.length; i++) {
+                if (this.cart[i] === id) {
+                    count++;
+                }
+            }
+            return count;
+        },
+    },
+    computed: {
+        cartItemCount() {
+            return this.cart.length || '';
+        },
+        sortedProducts() {
+            if (this.products.length > 0) {
+                let productsArray = this.products.slice(0);
+                function compare(a, b) {
+                    if (a.title.toLowerCase() < b.title.toLowerCase()) {
+                        return -1;
+                    }
+                    if (a.title.toLowerCase() > b.title.toLowerCase()) {
+                        return 1;
+                    }
+                    return 0;
+                }
+                return productsArray.sort(compare);
+            }
+        }
+    },
+    filters: {
+        formatPrice(price) {
+            if (!parseInt(price)) { return ""; }
+            if (price > 99999) {
+                let priceString = (price / 100).toFixed(2);
+                let priceArray = priceString.split("").reverse();
+                let index = 3;
+                while (priceArray.length > index + 3) {
+                    priceArray.splice(index+3, 0, ",");
+                    index += 4;
+                }
+                return "$" + priceArray.reverse().join("");
+            } else {
+                return "$" + (price / 100).toFixed(2);
+            }
+        }
+    },
+    created: function () {
+        axios.get('./products.json')
+            .then((response) => {
+                this.products = response.data.products;
+                console.log(this.products);
+            })
+    },
+});
